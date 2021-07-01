82bb3cebc04fa2ea078074db6689699a57eabf58
srdjansukovic <sukovic.ra3.2018@uns.ac.rs>
2021-07-01 13:41:33 +0200

    Promenjen stil naslovnice na pocetnoj strani.

---
+++ WebShopREST/WebContent/app/homepage.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+    el: '#cover',
+    data: {
+        showLoginModal: false,
+        showRegistrationModal: false
+    },
+    created() {
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    destroyed() {
+        window.removeEventListener('scroll', this.handleScroll);
+    },
+    mounted() {
+        
+    },
+    methods: {
+        handleScroll(event){
+            var nav = document.querySelector('nav');
+            if (window.pageYOffset > 100) {
+                nav.classList.add('navbar-custom', 'shadow');
+            } else {
+                nav.classList.remove('navbar-custom', 'shadow');
+            }
+        },
+    }
+});
