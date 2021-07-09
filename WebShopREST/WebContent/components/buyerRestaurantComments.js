98f4914109e508cc65e43845be6cf359fc3de41c
srdjansukovic <sukovic.ra3.2018@uns.ac.rs>
2021-07-09 19:20:13 +0200

    Odradjeni komentari za kupca.

--- WebShopREST/WebContent/components/buyerRestaurantComments.js
+++ WebShopREST/WebContent/components/buyerRestaurantComments.js
@@ -1,14 +1,7 @@
 Vue.component("restaurant-comments", {
     data: function() {
         return {
-            restaurant: null,
-            comments: [
-                {buyerUsername: 'Gorčilo', mark: 5, dateTime: '12.3.2021. 15:00', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
-                {buyerUsername: 'Slavko', mark: 3, dateTime: '15.3.2021. 15:00', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
-                {buyerUsername: 'Spiridon', mark: 4, dateTime: '12.7.2021. 15:00', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
-                {buyerUsername: 'Srki', mark: 4, dateTime: '1.2.2020. 15:00', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
-                {buyerUsername: 'Anci', mark: 5, dateTime: '23.8.2021. 15:00', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}
-            ]
+            comments: []
         }
     },
     template:         
@@ -31,7 +24,7 @@ Vue.component("restaurant-comments", {
                                         </div>
                                         <div class="col-md-auto"></div>
                                         <div class="col-md-3">
-                                            <span class="dateComment">{{c.dateTime}}</span>
+                                            
                                         </div>
                                     </div>
                                     <div class="row">
@@ -47,11 +40,10 @@ Vue.component("restaurant-comments", {
             </div>
         </div>`,
     mounted() {
-        let id = window.location.href.split('?')[1].split('=')[1].split('#/')[0];
-        axios.get('../rest/restaurants/' + id)
+        let restaurantId = window.location.href.split('?')[1].split('=')[1].split('#/')[0];
+        axios.get('../rest/comments/getAcceptedComments/' + restaurantId)
         .then(response => {
-            this.restaurant = response.data;
-
+            this.comments = response.data;
         });
     },
     methods: {
